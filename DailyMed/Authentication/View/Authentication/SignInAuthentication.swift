50c7881c73c509aeabdd924a92c5626e4e2f2867
Donovan Z <donovanzja@gmail.com>
2025-05-14 15:46:29 -0600

    Add SignIn and SignUp Authentication Views

--- DailyMed/Authentication/View/Authentication/SignInAuthentication.swift
+++ DailyMed/Authentication/View/Authentication/SignInAuthentication.swift
@@ -8,8 +8,72 @@
 import SwiftUI
 
 struct SignInAuthentication: View {
+    @State private var email: String = ""
+    @State private var errorEmail: String = ""
+    @State private var password: String = ""
+    @State private var errorPassword: String = ""
+    //@State private var isRedirectedToView: Bool = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .center){
+            Image("b1w")
+                .resizable()
+                .scaledToFill()
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+            VStack(spacing: 20) {
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 350, height: 560)
+                    .foregroundStyle(Color.white)
+                    .shadow(color: .gray.opacity( 0.6), radius: 10, x: 0, y:  6 )
+                    .overlay {
+                        VStack {
+                            VStack(alignment: .leading, spacing: 7) {
+                                Text("We ")
+                                    .foregroundStyle(Color.cBlue2)
+                                + Text("organize ")
+                                    .foregroundStyle(Color.cBlue1)
+                                + Text("your ")
+                                    .foregroundStyle(Color.cBlue2)
+                                + Text("medications")
+                                    .foregroundStyle(Color.cBlue1)
+                                
+                                Text("Get organized every day when you sign in")
+                                    .font(.headline)
+                                    .fontWeight(.regular)
+                                    .foregroundStyle(Color.cBlue2)
+                            }
+                            .font(.title2)
+                            .bold()
+                            .padding(.top, 30)
+                            
+                            TextFieldsAuthentication(email: $email, errorEmail: $errorEmail, password: $password, errorPassword: $errorPassword)
+                                .padding(.top, 20)
+                            SignInButton()
+                                .padding(.top, 35)
+                            Text("or sign in with")
+                                .font(.system(.footnote, design: .default, weight: .regular))
+                                .foregroundStyle(Color.cBlue3)
+                                .padding(.top, 15)
+                            GoogleAuthenticationButton()
+                                .padding(.top, 15)
+                            Spacer()
+                            Text("I don´t have an account")
+                                .font(.system(.footnote, design: .default, weight: .regular))
+                                .foregroundStyle(Color.cBlue3)
+                                .padding(.bottom, 20)
+                            
+                        }
+                        .padding(.horizontal, 20)
+                        
+                    }
+                    .padding(.top, 60)
+                SignUpButton(doesItRegister: false)
+                    .frame(width: 350)
+            }
+            
+        }
+        .ignoresSafeArea()
+        
     }
 }
 
--- DailyMed/Authentication/View/Authentication/SignUpAuthentication.swift
+++ DailyMed/Authentication/View/Authentication/SignUpAuthentication.swift
@@ -8,8 +8,66 @@
 import SwiftUI
 
 struct SignUpAuthentication: View {
+    @State private var email: String = ""
+    @State private var errorEmail: String = ""
+    @State private var password: String = ""
+    @State private var errorPassword: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .center){
+            Image("BackgroundPills")
+                .resizable()
+                .scaledToFill()
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+            VStack(spacing: 20) {
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 350, height: 560)
+                    .foregroundStyle(Color.white)
+                    .shadow(color: .gray.opacity( 0.6), radius: 10, x: 0, y:  6 )
+                    .overlay {
+                        VStack {
+                            VStack(alignment: .leading, spacing: 7) {
+                                Text("We ")
+                                    .foregroundStyle(Color.cBlue2)
+                                + Text("organize ")
+                                    .foregroundStyle(Color.cBlue1)
+                                + Text("your ")
+                                    .foregroundStyle(Color.cBlue2)
+                                + Text("medications")
+                                    .foregroundStyle(Color.cBlue1)
+                                
+                                Text("Get organized every day when you sign in")
+                                    .font(.headline)
+                                    .fontWeight(.regular)
+                                    .foregroundStyle(Color.cBlue2)
+                            }
+                            .font(.title2)
+                            .bold()
+                            .padding(.top, 30)
+                            
+                            TextFieldsAuthentication(email: $email, errorEmail: $errorEmail, password: $password, errorPassword: $errorPassword)
+                                .padding(.top, 20)
+                            SignUpButton(doesItRegister: true )
+                                .padding(.top, 35)
+                            Text("or sign up with")
+                                .font(.system(.footnote, design: .default, weight: .regular))
+                                .foregroundStyle(Color.cBlue3)
+                                .padding(.top, 15)
+                            GoogleAuthenticationButton()
+                                .padding(.top, 15)
+                            Spacer()
+                            
+                        }
+                        .padding(.horizontal, 20)
+                        
+                    }
+                    .padding(.top, 60)
+            }
+            
+        }
+        .ignoresSafeArea()
+        
+        
     }
 }
 
