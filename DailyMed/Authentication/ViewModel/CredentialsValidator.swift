fe1529009417065efa3b2795b548d27c06f6583d
Donovan Z <donovanzja@gmail.com>
2025-05-30 14:03:46 -0600

    Add CredentialsValidator in ViewModel carpet

--- DailyMed/Authentication/ViewModel/CredentialsValidator.swift
+++ DailyMed/Authentication/ViewModel/CredentialsValidator.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+protocol CredentialsValidating {
+    func verifyValidEmail(email: String) throws -> Bool
+    func verifyValidPassword(password: String) throws -> Bool
+}
+
+class CredentialsValidator: CredentialsValidating {
+    func verifyValidEmail(email: String) throws -> Bool {
+        guard email.isValidEmail else {
+            throw SignUpError.invalidEmail
+        }
+        return true
+    }
+    
+    func verifyValidPassword(password: String) throws -> Bool {
+        guard password.isSecurityPasswordValid else  {
+            throw SignUpError.invalidPassword
+        }
+        return true
+    }
+    
+}
