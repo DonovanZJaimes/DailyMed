ee54925f80e6282655f5c7c5198a887787c8e396
Donovan Z <donovanzja@gmail.com>
2025-05-30 14:02:36 -0600

    Add Authentication ViewModel

--- DailyMed/Authentication/ViewModel/AuthenticationViewModel.swift
+++ DailyMed/Authentication/ViewModel/AuthenticationViewModel.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+class AuthenticationViewModel: ObservableObject {
+    @Published var email: String = ""
+    @Published var errorEmail: String = ""
+    @Published var password: String = ""
+    @Published var errorPassword: String = ""
+    
+    private let validator: CredentialsValidating
+    
+    init(validator: CredentialsValidating = CredentialsValidator()){
+        self.validator = validator
+    }
+    
+    func verifyCredentials(withEmail email: String, andPassword password: String) -> Bool {
+        var isEmailValid: Bool = false
+        var isPasswordValid: Bool = false
+        do {
+            isEmailValid = try validator.verifyValidEmail(email: email)
+            errorEmail = ""
+        }catch {
+            errorEmail = error.localizedDescription
+        }
+        
+        do {
+            isPasswordValid = try validator.verifyValidPassword(password: password)
+            errorPassword = ""
+        } catch {
+            errorPassword = error.localizedDescription
+        }
+        
+        
+        return isPasswordValid && isEmailValid
+    }
+    
+    func cleanCredentials() {
+        email = ""
+        password = ""
+        errorEmail = ""
+        errorPassword = ""
+    }
+    
+    
+    
+}
